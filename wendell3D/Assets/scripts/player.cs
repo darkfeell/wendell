5a896f43f2f87dab30f62dad7333b98c612753e5
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2023-10-27 16:10:39 -0300

    animations input and some other things that i forgot

--- wendell3D/Assets/scripts/player.cs
+++ wendell3D/Assets/scripts/player.cs
@@ -6,13 +6,17 @@ public class player : MonoBehaviour
 {
     public float speed;
     private CharacterController controller;
+    Vector3 moveDirection;
     public float smoothRotTime;
     private float turnSmoothVelocity;
+    public float gravity;
+    private Animator anim;
 
     public Transform cam;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
         cam = Camera.main.transform;
     }
@@ -20,19 +24,47 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        Move();
+    }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
-        
 
-        if(direction.magnitude > 0)
+    private void Move()
+    {
+        if (controller.isGrounded)
         {
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-            Vector3 moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-            controller.Move(moveDirection * speed * Time.deltaTime);
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+
+            if (direction.magnitude > 0)
+            {
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+                moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+                anim.SetInteger("transition", 2);
+            }
+            else
+            {
+                anim.SetInteger("transition", 0);
+                moveDirection = Vector3.zero;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    void GetMouseInput()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetInteger("transition", 3);
+            }
         }
     }
 }
