e4417f87a33e100120318bd3ed65864284101a7b
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2023-11-03 16:27:15 -0300

    enemy combat halfway made and enemy animations (pending player animation fix)

--- wendell3D/Assets/scripts/EnemyCombat.cs
+++ wendell3D/Assets/scripts/EnemyCombat.cs
@@ -10,14 +10,18 @@ public class EnemyCombat : MonoBehaviour
     public float attackDamage;
     public float moveSpeed;
     public float lookRadius;
+    public float colliderRadius;
 
     [Header("Components")]
     private Animator e_anim;
     private CapsuleCollider capColl;
     private NavMeshAgent meshAgent;
 
-    [Header("Components")]
+    [Header("Others")]
     private Transform player;
+    private bool walking;
+    private bool attacking;
+    private bool waitFor;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,7 +39,29 @@ public class EnemyCombat : MonoBehaviour
 
         if(distance <= lookRadius)
         {
-
+            meshAgent.isStopped = false;
+            if (!attacking)
+            {
+                meshAgent.SetDestination(player.position);
+                e_anim.SetBool("Walk Forward", true);
+                walking = true;
+            }
+            
+            if(distance <= meshAgent.stoppingDistance)
+            {
+                StartCoroutine("Attack");
+            }
+            else
+            {
+                attacking = false;
+            }
+        }
+        else
+        {
+            meshAgent.isStopped = true;
+            e_anim.SetBool("Walk Forward", false);
+            walking = false;
+            attacking = false;
         }
     }
 
@@ -43,5 +69,36 @@ public class EnemyCombat : MonoBehaviour
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    IEnumerator Attack()
+    {
+        if (!waitFor)
+        {
+            waitFor = true;
+            attacking = true;
+            walking = false;
+            e_anim.SetBool("Walk Forward", false);
+            e_anim.SetBool("Bite Attack", true);
+            yield return new WaitForSeconds(1.2f);
+
+            GetPlayer();
+
+
+           // yield return new WaitForSeconds(1f);
+            waitFor = false;
+        }
+        
+    }
+
+    void GetPlayer()
+    {
+        foreach(Collider coll in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
+        {
+            if (coll.gameObject.CompareTag("Player"))
+            {
+
+            }
+        }
     }
 }
